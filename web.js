4f1c7b4215a98d32201337893d4d13c22d313e65
wgrmmtmr <wgrmmtmr@gmail.com>
2013-07-14 09:37:21 +0800

    first page

--- web.js
+++ web.js
@@ -5,6 +5,7 @@ var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
+
   var buffer = new Buffer(100);
   buffer = fs.readFileSync("index.html");
   var string = buffer.toString('utf8');
